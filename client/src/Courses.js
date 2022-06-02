95ceb696d49e35155a79f4915a8e394010769828
Anurag Goel <b119011@iiit-bh.ac.in>
2022-06-02 19:28:25 +0530

    testing changes

--- client/src/Courses.js
+++ client/src/Courses.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CourseContent from "./components/CourseContent";
+//random change
 function Courses(){
     return (
         <div>
