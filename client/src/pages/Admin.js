5e4ed406d48873cd7ff208df07ac0a0bfc205eef
Anurag Goel <b119011@iiit-bh.ac.in>
2022-06-04 19:33:42 +0530

    fixed redirecting in admin page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,40 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { Navigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import AdminContent from "../components/AdminContent";
 import axios from "axios";
-function Admin(){
-    const [allowed,setAllowed] = useState(false);
+function Admin() {
+    const [allowed, setAllowed] = useState(false);
 
-    useEffect(()=>{
-        const allow =async()=>{
-            const response= await axios.get('http://localhost:5000/admin',{
-                withCredentials:true,
-                headers:{
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Credentials":true,
-                }
-            })
-            if(response.status===200){
-                console.log("Yaay!")
-                setAllowed(true);
-            }
-        }
+    useEffect(() => {
+        const allow = async () => {
+            await axios
+                .get("http://localhost:5000/admin", {
+                    withCredentials: true,
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Credentials": true,
+                    },
+                })
+                .then((res) => {
+                    if (res.status === 200) {
+                        setAllowed(true);
+                    }
+                })
+                .catch((error) => {
+                    window.alert("You need to login to access the page");
+                    window.location = "http://localhost:3000";
+                });
+        };
         allow();
-    },[]);
-    
+    }, []);
+
     return (
         <div>
-        <Header/>
-        <AdminContent/>
-        {
-            allowed ? <div className="">hello</div>: console.log("not allowed")
-        }
-        <Footer/>
+            <Header />
+            {allowed ? <AdminContent /> : <div style={{marginTop:"10rem"}}>"loading..."</div>}
+            <Footer />
         </div>
-    )
+    );
 }
 
 export default Admin;
