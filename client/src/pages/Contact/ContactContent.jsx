229f89ae5e007b1197dd2c0a5fcf2ed5bc835e09
Anurag Goel <b119011@iiit-bh.ac.in>
2022-06-08 19:23:26 +0530

    Created a single component for top section of multiple pages

--- client/src/pages/Contact/ContactContent.jsx
+++ client/src/pages/Contact/ContactContent.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import {ContactDetails} from "./Contactdetails";
 import ContactCard from "./ContactCard";
+import Top from "../../components/Top";
+import { TopData } from "../../components/Topdata";
 
 function contactContent() {
   return (
-    <div className="contact-body">
-      <div className="contact-content">
-        <h1 className="contact-heading">Contact Us</h1>
-        <p className="contact-paragraph">
-          If you have any queries, complaints or suggestions, make sure to
-          contact us.
-        </p>
-        <img className="contact-main-image" src="/images/contact.png"></img>
-      </div>
+    <div className="contact-container">
+      <Top heading={TopData[2].heading} paragraph={TopData[2].paragraph} image={TopData[2].image}/>
       <div className="contact-cards">
        {ContactDetails.map((contact,index)=>{
         return <ContactCard key={index} title={contact.title} className="contact-card" details={contact.details} image={contact.image}/>
