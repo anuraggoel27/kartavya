ba4b679fc549b85a4d59df77585ed8fa94af87b1
Anurag Goel <b119011@iiit-bh.ac.in>
2022-06-08 19:48:47 +0530

    Final commit on css of pages

--- client/src/pages/Gallery/GalleryContent.jsx
+++ client/src/pages/Gallery/GalleryContent.jsx
@@ -3,18 +3,31 @@ import Top from "../../components/Top";
 import { TopData } from "../../components/Topdata";
 import GalleryCard from "./GalleryCard";
 import links from "./galleryPhotosLink";
-import "./styles.css"
+import "./styles.css";
 
 function GalleryContent() {
-  return (
-    <div className="achievement-container">
-        <Top heading={TopData[4].heading} paragraph={TopData[4].paragraph} image={TopData[4].image}/>
-        <div className="gallery-card-section">
-      {links.map(function(Link, index){
-        return <GalleryCard className="gallery-card" key={index} img={Link.imgUrl} text={Link.description} />
-      })}
-      </div>
-    </div>
-  );
+    return (
+        <div className="achievement">
+            <Top
+                heading={TopData[4].heading}
+                paragraph={TopData[4].paragraph}
+                image={TopData[4].image}
+            />
+            <div className="gallery-content">
+                <div className="gallery-card-section">
+                    {links.map(function (Link, index) {
+                        return (
+                            <GalleryCard
+                                className="gallery-card"
+                                key={index}
+                                img={Link.imgUrl}
+                                text={Link.description}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
 }
 export default GalleryContent;
